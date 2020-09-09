65562d743a666387c0ffdaf3d2127f7dee6174f6
Abalth <59120031+Abalth@users.noreply.github.com>
2020-09-09 14:48:58 +0200

    Single layer perceptron

--- ANNDA_1.py
+++ ANNDA_1.py
@@ -16,6 +16,22 @@ def linear(n, m1,m2, sigma1, sigma2):
     return classA, classB
 
 
+# As defined in the lab 
+def non_linear(n, m1,m2, sigma1, sigma2):
+    
+    classA = np.zeros((n,2))
+    
+    temp1 = (np.random.normal(-m1[0], sigma1, size=[int(round(0.5*n)),1]))
+    temp2 = (np.random.normal(m1[0], sigma1, size=[int(round(0.5*n)),1]))
+        
+    temp_flat = np.squeeze(np.concatenate((temp1, temp2), axis=0), axis=1)
+    classA[:,0] = temp_flat
+
+    classA[:,1] = np.squeeze(np.random.normal(m1[1], sigma1, size=[n,1]), axis=1)   
+    classB = np.random.normal(m2, sigma2, size=[n,2])
+    return classA, classB
+
+
 def plot_data(dataA, dataB): 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dataA[:,0], y=dataA[:,1], mode="markers"))
